e81e1f693c69519150c5b1460ae3de7d79ebf55d
desarrolloenprg <36509448+desarrolloenprg@users.noreply.github.com>
2020-12-24 10:57:50 -0500

    Add files via upload

---
+++ Discourse/GoogleSheetsHelper.cs
@@ -0,0 +1,84 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Dynamic;
+
+namespace Discourse
+{
+    public class GoogleSheetsHelper
+    {
+        static string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static string ApplicationName = "GoogleSheetsHelper";
+
+        private readonly SheetsService _sheetsService;
+        private readonly string _spreadsheetId;
+
+        public GoogleSheetsHelper(string credentialFileName, string spreadsheetId)
+        {
+            var credential = GoogleCredential.FromStream(new FileStream(credentialFileName, FileMode.Open)).CreateScoped(Scopes);
+
+            _sheetsService = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            _spreadsheetId = spreadsheetId;
+        }
+
+        public void AddCells(GoogleSheetParameters googleSheetParameters, List<GoogleSheetRow> rows)
+        {
+            var requests = new BatchUpdateSpreadsheetRequest { Requests = new List<Request>() };
+
+            var sheetId = GetSheetId(_sheetsService, _spreadsheetId, googleSheetParameters.SheetName);
+
+            GridCoordinate gc = new GridCoordinate
+            {
+                ColumnIndex = googleSheetParameters.RangeColumnStart - 1,
+                RowIndex = googleSheetParameters.RangeRowStart - 1,
+                SheetId = sheetId
+            };
+
+            var request = new Request { UpdateCells = new UpdateCellsRequest { Start = gc, Fields = "*" } };
+
+            var listRowData = new List<RowData>();
+
+            foreach (var row in rows)
+            {
+                var rowData = new RowData();
+                var listCellData = new List<CellData>();
+                foreach (var cell in row.Cells)
+                {
+                    var cellData = new CellData();
+                    var extendedValue = new ExtendedValue { StringValue = cell.CellValue };
+
+                    cellData.UserEnteredValue = extendedValue;
+                    var cellFormat = new CellFormat { TextFormat = new TextFormat() };
+
+                    if (cell.IsBold)
+                    {
+                        cellFormat.TextFormat.Bold = true;
+                    }
+
+                    cellFormat.BackgroundColor = new Color { Blue = (float)cell.BackgroundColor.B / 255, Red = (float)cell.BackgroundColor.R / 255, Green = (float)cell.BackgroundColor.G / 255 };
+
+                    cellData.UserEnteredFormat = cellFormat;
+                    listCellData.Add(cellData);
+                }
+                rowData.Values = listCellData;
+                listRowData.Add(rowData);
+            }
+            request.UpdateCells.Rows = listRowData;
+
+            // It's a batch request so you can create more than one request and send them all in one batch. Just use reqs.Requests.Add() to add additional requests for the same spreadsheet
+            requests.Requests.Add(request);
+
+            _sheetsService.Spreadsheets.BatchUpdate(requests, _spreadsheetId).Execute();
+        }
+    }
+ }
---
+++ Discourse/Items.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Discourse
+{
+    internal class User
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string name { get; set; }
+        public string avatar_template { get; set; }
+        public string title { get; set; }
+
+    }
+
+    internal class DirectoryItem
+    {
+        public int id { get; set; }
+        public int likes_received { get; set; }
+        public int likes_given { get; set; }
+        public int topics_entered { get; set; }
+        public int topic_count { get; set; }
+        public int post_count { get; set; }
+        public int posts_read { get; set; }
+        public int days_visited { get; set; }
+        public User user { get; set; }
+
+    }
+
+    internal class Meta
+    {
+        public DateTime last_updated_at { get; set; }
+        public int total_rows_directory_items { get; set; }
+        public string load_more_directory_items { get; set; }
+
+    }
+
+    internal class Items
+    {
+        public List<DirectoryItem> directory_items { get; set; }
+        public Meta meta { get; set; }
+
+    }
+}
